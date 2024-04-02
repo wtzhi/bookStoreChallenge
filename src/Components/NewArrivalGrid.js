32eb59a0078024a9a2756dee792c4f4646d7905b
wtzhi <weitaozhi001@gmail.com>
2024-04-03 10:37:45 +1300

    update scrolltop
    color change

--- src/Components/NewArrivalGrid.js
+++ src/Components/NewArrivalGrid.js
@@ -4,13 +4,14 @@ import { useEffect, useContext, useState, useRef } from 'react'
 import Grid from '@mui/material/Grid';
 import Loading from '../Components/Loading';
 import { BookContext } from '../BookContextProvider';
+import { borderRadius } from '@mui/system';
 
 export const NewArrivalGrid = () => {
     const {books, booksLoading,refreshBooks,err} = useContext(BookContext);
 
     
   return (
-    <div style = {{position:"relative",left: "50%",transform: "translate(-50%)",marginTop:"20px", backgroundColor:"#f5f5f5", width:"90vw"}}>
+    <div style = {{position:"relative",left: "50%",transform: "translate(-50%)",marginTop:"20px", backgroundColor:"#f5f5f5", width:"90vw",borderRadius:"50px"}}>
         <p style = {{position:"relative",fontFamily: "Georgia, serif",fontSize:"30px"}}>New Arrival</p>
     {books ? 
         <Grid  sx={{ flexGrow: 1, marginTop:"20px", direction:"row"}} container spacing={1} columns={5}>
